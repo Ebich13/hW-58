3cd44765fcacae2e3c02ab0063175ff6305b83c5
Kubat <kubas9214@gmail.com>
2023-11-14 04:52:55 +0600

    Final version of code!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
+import React, {useState} from 'react';
+import Toolbar from './Componets/Toolbar/Toolbar';
+import Modal from './Componets/Modal/Modal';
+import Alert from './Componets/Alert/Alert';
+
+
 function App() {
+  const [showModal, setShowModal] = useState(true);
+  const [showAlert, setShowAlert] = useState(true);
+
+
+  const modalButtons = [
+    { type: 'primary', label: 'Continue', onClick:() => alert("You are ready?") },
+    { type: 'danger', label: 'Close', onClick:() => setShowModal(false)},
+  ];
+
   return (
-      <div>
-      test
+    <>
+      <header>
+        <Toolbar/>
+      </header>
+      <div className="container mt-5">
+        <button className="btn btn-primary" onClick={() => setShowModal(true)}>
+          Open Modal
+        </button>
+        {showModal && (
+          <Modal show={showModal} onClose={() => setShowModal(false)} title="Some kinda modal title" buttons={modalButtons}>
+            <p>This is modal content</p>
+          </Modal>
+        )}
+
+        <Alert  type="success" onDismiss={() =>setShowAlert(false)}>
+          This is a success type alert
+        </Alert>
+        {showAlert && (
+          <Alert type="warning" clickDismissible onDismiss={() => setShowAlert(false)}>
+            This is a warning type alert
+          </Alert>
+        )}
       </div>
+    </>
   );
 }
+
+
 
 export default App;
